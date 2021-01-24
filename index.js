a28ef5dd214067ee13bf623da766c38520c5609e
racross1 <racross1@gmail.com>
2021-01-24 15:36:44 -0800

    commit tree 8949ffffead4cd5fc28263cb088232a791d6676e

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // Your code here
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(thing = 'go to the office') {
+    return `This Monday, I will ${thing}.`
+}
+function wrapAdjective(flair ='*'){
+    return function (param = 'special') { 
+        return `You are ${flair}${param}${flair}!`
+    }
+}
+
+let Calculator = {
+    add: function(a,b){
+        return a+b
+    },
+    subtract: function(a,b){
+        return a-b
+    },
+    multiply: function(a,b){
+        return a * b
+    },
+    divide: function(a,b){
+        return a/b
+    }
+}   
+
+function actionApplyer(starter, arr) {
+    let a = starter 
+    arr.forEach(f => {
+        a = f(a)
+    })
+    return a
+}
+
+    // function actionApplyer(starter,aray) {
+    //     let a = starter;
+      
+    //     for (let i = 0; i < aray.length; i++ ){
+    //       a = aray[i](a);
+    //     }
+    //     return a  ;
+    //   }
